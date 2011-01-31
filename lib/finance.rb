bea778d5d2139f6048ed98239a88f216122695ee
Justin Tyler Wiley <justin.wiley@gmail.com>
2011-01-31 18:30:37 -0500

    adds present value of annuity

--- lib/finance.rb
+++ lib/finance.rb
@@ -1,13 +1,17 @@
 module Finance
-  def pv(v,r,n)
-    v/((1+r)**n)
+  def pv(v,r,n,m=1)
+    v/((1+r/m)**n)
   end
 
-  def fv(v,r,n)
-    v*((1+r)**n)
+  def fv(v,r,n,m=1)
+    v*((1+r*m)**n)
   end
   
-  def npv(v1,v2,r,n)
-    pv(v1,r,n) - pv(v2,r,n)
+  def npv(v1,v2,r,n,m1=1,m2=1)
+    pv(v1,r,n,m1) - pv(v2,r,n,m2)
+  end
+  
+  def pva(a,i,n)
+    a*(1-((1+i)**-n))/i
   end
 end
--- spec/finance_spec.rb
+++ spec/finance_spec.rb
@@ -19,7 +19,8 @@ describe Finance do
   describe "Present Value" do
     it "should not allow r's higher than 1"
     it "should range time scale -1 < n"
-    
+    it "should assume period 2 given no time value" 
+
     it "accepts a number, discount rate, and time period and return a time discounted value" do
       pv(10,0.1,2).r.should == 8.2645 
     end
@@ -27,11 +28,14 @@ describe Finance do
     it "should compound discount rates" do
       pv(10,0.1,3).r.should == 7.5131
     end
-    
-    it "should assume period 2 given no time value" do
-      pending
-      pv().should == 1
+ 
+    it "should compound discount rates" do
+      pv(10,0.1,3).r.should == 7.5131
     end
+ 
+    it "should accept an optional period per annum" do
+      pv(10,0.1,3,2).r.should == 8.6384
+    end    
   end
   
   describe "Future Value" do
@@ -42,6 +46,10 @@ describe Finance do
     it "should compound discount rates" do
       fv(10,0.1,2).r.should == 12.1000
     end
+    
+    it "should accept an optional period per annum" do
+      fv(10,0.1,2,2).r.should == 14.4
+    end    
   end
   
   describe "Net Present Value" do
@@ -49,5 +57,16 @@ describe Finance do
       npv(15,10,0.1,1).r.should == 4.5455
       #12.39 - 8.26
     end
+    
+    it "should accept two optional periods per annum" do
+      npv(15,10,0.1,1,2,2).r.should == 4.7619
+    end
   end
+  
+  describe "Present Value of Annuity" do
+    it "should accept annuity payment, interest rate, and period and return pv of annuity" do
+      pva(1000,0.1,5).r.should == 3790.7868
+    end
+  end
+
 end
