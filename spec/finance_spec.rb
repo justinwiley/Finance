21b33bb09917683baf0ab50639e5769d8f07db08
Justin Tyler Wiley <justin.wiley@gmail.com>
2011-01-31 12:35:39 -0500

    fighting rspec

---
+++ spec/finance_spec.rb
@@ -0,0 +1,9 @@
+$: << '../'
+
+require 'finance.rb'
+
+describe Finance do
+  it "should blah" do
+    Finance.blah.should == 1
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+$: << '.' # for ruby 1.9
+require 'rubygems'
+require 'spec'
+require 'spec/mocks'
+require 'spec/runner/differs/default'
+require 'spec/autorun'
+
+require 'support/spec_classes'
+require 'support/macros'
